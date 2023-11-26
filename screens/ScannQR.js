c7078dcfe1db258a92055d5b00e370ed25199ee2
KMargarjan <150011395+KMargarjan@users.noreply.github.com>
2023-11-26 15:55:28 +0100

    feat: scan QR code and cancel QR

--- screens/ScannQR.js
+++ screens/ScannQR.js
@@ -1,5 +1,125 @@
-import { Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { BarCodeScanner } from "expo-barcode-scanner";
+import ScannedData from "./ScannedData";
+import { useNavigation } from "@react-navigation/native";
 
-export default function ScannQr() {
-  return <Text>Scann</Text>;
+export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [scannedData, setScannedData] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const unsubscribeBlur = navigation.addListener("blur", () => {
+      // Screen is blurred (navigated away from)
+      setScanned(false);
+      setScannedData(null);
+    });
+
+    return () => {
+      // Cleanup the event listeners
+      unsubscribeBlur();
+    };
+  }, [navigation]);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    if (data) {
+      setScannedData(data);
+      setScanned(false);
+    }
+  };
+
+  const renderCamera = () => {
+    return (
+      <View style={styles.cameraContainer}>
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+          style={styles.camera}
+        />
+      </View>
+    );
+  };
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+
+  if (hasPermission === false) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Camera permission not granted</Text>
+      </View>
+    );
+  }
+  const resetScann = () => {
+    setScannedData(null);
+    setScanned(false);
+  };
+
+  const addToList = () => {};
+
+  return (
+    <View style={styles.container}>
+      {!scannedData && <Text style={styles.title}>Please scann QR Code</Text>}
+      {!scannedData && renderCamera()}
+
+      {scannedData && (
+        <Text>
+          <ScannedData
+            data={scannedData}
+            onCancel={resetScann}
+            onAddToList={addToList}
+          />
+        </Text>
+      )}
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  paragraph: {
+    fontSize: 16,
+    marginBottom: 40,
+  },
+  cameraContainer: {
+    width: "80%",
+    aspectRatio: 1,
+    overflow: "hidden",
+    borderRadius: 10,
+    marginBottom: 40,
+  },
+  camera: {
+    flex: 1,
+  },
+  button: {
+    backgroundColor: "blue",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
---
+++ screens/ScannedData.js
@@ -0,0 +1,54 @@
+import { Text, View, StyleSheet, Button } from "react-native";
+
+export default function ScannedData({ data, onCancel, onAddToList }) {
+  return (
+    <View style={styles.card}>
+      <View style={styles.cardContent}>
+        <Text style={styles.title}>{data}</Text>
+        <Text style={styles.description}>{data}</Text>
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <Button title="Cancel" onPress={onCancel} />
+        <View style={styles.space} />
+        <Button title="Add to list" onPress={onAddToList} />
+      </View>
+    </View>
+  );
+}
+
+// Styles
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: "#fff",
+    borderRadius: 8,
+    marginVertical: 5,
+    marginHorizontal: 10,
+    padding: 20, // Reduced padding for better layout
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+    elevation: 3,
+    height: 200,
+  },
+  cardContent: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  description: {
+    fontSize: 14,
+    color: "#333",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 10,
+  },
+  space: {
+    width: 20,
+  },
+});
